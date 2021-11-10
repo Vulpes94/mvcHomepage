9624e383126171470724d8322b4926f0587d482a
Vulpes94 <kjunfi94@gmail.com>
2021-11-10 17:53:27 +0900

    게시판 수정

---
+++ src/com/java/board/command/UpdateOkCommand.java
@@ -0,0 +1,35 @@
+package com.java.board.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.java.board.dao.BoardDao;
+import com.java.board.dto.BoardDto;
+import com.java.command.Command;
+import com.java.logger.MyLogger;
+
+public class UpdateOkCommand implements Command {
+
+  @Override
+  public String proRequest(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+    
+    request.setCharacterEncoding("utf-8");
+    int pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
+    
+    BoardDto boardDto = new BoardDto();
+    boardDto.setBoardNumber(Integer.parseInt(request.getParameter("boardNumber")));
+    boardDto.setSubject(request.getParameter("subject"));
+    boardDto.setEmail(request.getParameter("email"));
+    boardDto.setContent(request.getParameter("content"));
+    boardDto.setPassword(request.getParameter("password"));
+    MyLogger.logger.info(MyLogger.logMsg + boardDto.toString());
+    MyLogger.logger.info(MyLogger.logMsg + "pageNumber:"+ pageNumber);
+    
+    int check = BoardDao.getInstance().update(boardDto);
+    MyLogger.logger.info(MyLogger.logMsg + check);
+    
+    request.setAttribute("check", check);
+    request.setAttribute("pageNumber", pageNumber);
+    return "/WEB-INF/views/board/updateOk.jsp";
+  }
+
+}
